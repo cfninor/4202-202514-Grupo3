f99cd0172126ddde95393e8a92c1424185f41df2
Carlos Niño <cf.ninor1@uniandes.edu.co>
2025-09-25 22:48:02 -0500

    Se actualiza app para heroku

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 db_url = os.environ.get("DATABASE_URL")
 if not db_url:
     raise RuntimeError("DATABASE_URL no está definido. Revisa tu .env")
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql+psycopg2://", 1)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
